7f0569ac1f0c858d1fd3b81e4df675ab919f3b39
QuanBlue <quannguyenthanh558@gmail.com>
2023-04-05 21:39:49 +0700

    update: print UI

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 import grpc
-
+import os
 import service.chat_pb2 as chat_pb2
 import service.chat_pb2_grpc as chat_pb2_grpc
 
@@ -12,13 +12,19 @@ def run():
 
     while True:
         text = input("Enter your message: ")
+
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+        print(f"Enter your name: {user_name}")
+        print("-------------- CHAT BOX - gRPC --------------")
         message = chat_pb2.Message(user_name=user_name, text=text)
         response = stub.SendMessage(message)
-        print(f"{response.user_name}: {response.text}")
 
         messages = stub.ReceiveMessage(chat_pb2.Empty())
         for message in messages:
             print(f"{message.user_name}: {message.text}")
+
+        print('---------------------------------------------')
 
 
 if __name__ == '__main__':
