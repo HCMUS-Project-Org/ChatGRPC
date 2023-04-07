1a2fc1b82085183a64b1d112b4a1137adc54ad49
QuanBlue <quannguyenthanh558@gmail.com>
2023-04-07 07:41:24 +0700

    feat: LIKE msg to allow continues chat

--- client.py
+++ client.py
@@ -5,6 +5,26 @@ import service.chat_pb2_grpc as chat_pb2_grpc
 import datetime
 
 
+def IsLikeMessage(msg):
+    msg_components = msg.split('_')
+    if len(msg_components) == 2 and msg_components[0] == 'LIKE' and msg_components[1].isdigit():
+        return True
+    return False
+
+
+def ClearScreen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def ShowMessage(stub):
+    # receive all msg from server
+    messages = stub.ReceiveMessage(chat_pb2.Empty())
+    for message in messages:
+        if not IsLikeMessage(message.msg):
+            print(
+                f"[{message.time}][{message.user.id}] {message.user.name}: {message.msg}")
+
+
 def run():
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = chat_pb2_grpc.ChatServiceStub(channel)
@@ -14,31 +34,41 @@ def run():
         user = chat_pb2.User(name=user_name)
 
         while True:
-            msg = input("Enter your message: ")
+            try:
+                msg = input("Enter your message: ")
 
-            os.system('cls' if os.name == 'nt' else 'clear')
+                ClearScreen()
+                print(f"Enter your name: {user_name}")
+                print("-------------- CHAT BOX - gRPC --------------")
 
-            print(f"Enter your name: {user_name}")
-            print("-------------- CHAT BOX - gRPC --------------")
+                # get current time
+                current_time = datetime.datetime.now()
+                formatted_time = current_time.strftime("%H:%M:%S")
 
-            # get current time
-            current_time = datetime.datetime.now()
-            formatted_time = current_time.strftime("%H:%M:%S")
+                # send msg to server
+                message = chat_pb2.Message(
+                    user=user, msg=msg, time=formatted_time)
+                response = stub.SendMessage(message)
 
-            # send msg to server
-            message = chat_pb2.Message(user=user, msg=msg, time=formatted_time)
-            response = stub.SendMessage(message)
+                # update user id if it is not set
+                if not user.id:
+                    user.id = response.user.id
+                # print("[Client - Response(SendMsg)]: ", response)
 
-            # update user id if it is not set
-            if not user.id:
-                user.id = response.user.id
-            print("[Client - Response(SendMsg)]: ", response)
+                ShowMessage(stub)
+            except grpc.RpcError as e:
+                error_details = e.details().strip("Exception calling application: ")
 
-            # receive all msg from server
-            messages = stub.ReceiveMessage(chat_pb2.Empty())
-            for message in messages:
-                print(
-                    f"[{message.time}][{message.user.id}] {message.user.name}: {message.msg}")
+                error = error_details.split("_")[0]
+                id = error_details.split("_")[1]
+                # update user id if it is not set
+                if not user.id:
+                    user.id = id
+
+                ShowMessage(stub)
+                print('---------------------------------------------')
+                print(error)
+                continue
 
             print('---------------------------------------------')
 
--- server.py
+++ server.py
@@ -9,23 +9,140 @@ import service.chat_pb2_grpc as chat_pb2_grpc
 class ChatServiceServicer(chat_pb2_grpc.ChatServiceServicer):
     def __init__(self):
         self.messages = []
+        self.allow_users = [
+            {"user_id": "00", "like_count": 0, "like_from": [], "is_allow": True}]
         self.max_user_id = 0
 
-    def SendMessage(self, request, context):
-        print('--------- Server - Send Request ---------------------')
-        print("--- [original]: \n", request)
-        # Increment the user ID if it is not set or if it is lower than the current max ID
-        if not request.user.id:
-            request.user.id = str(self.max_user_id + 1)
-            self.max_user_id += 1
+    def GenerateUserId(self):
+        print("[GENERATE] User ID:", end=" ")
+        new_id = str(self.max_user_id + 1)
 
         # Add a leading zero to the user ID if it is less than 10
-        if len(request.user.id) < 2:
-            request.user.id = '0' + request.user.id
+        if len(new_id) < 2:
+            new_id = '0' + new_id
 
-        print("--- [original]: \n", request)
+        self.max_user_id += 1
+        print(new_id)
 
-        self.messages.append(request)
+        return new_id
+
+    def CreateNewUser(self):
+        print("[CREATE] New user:")
+
+        new_id = self.GenerateUserId()
+        new_user = {"user_id": new_id, 'like_count': 0,
+                    'like_from': [], 'is_allow': True}
+
+        print("  {")
+        print("    user_id:", new_user["user_id"])
+        print("    like_count:", new_user["like_count"])
+        print("    like_from:", new_user["like_from"])
+        print("    is_allow:", new_user["is_allow"])
+        print("  }")
+
+        # Add the user to the list of allowed users
+        self.allow_users.append(new_user)
+
+        return new_id
+
+    def IsLikeMessage(self, msg):
+        print("[CHECK] Is LIKE message:", end=" ")
+
+        msg_components = msg.split('_')
+
+        if len(msg_components) == 2 and msg_components[0] == 'LIKE' and msg_components[1].isdigit():
+            print("True")
+            return True
+
+        print("False")
+        return False
+
+    def HandleLikeMessage(self, msg, from_user):
+        print("[HANDLE] LIKE message")
+
+        msg_components = msg.split('_')
+        user_id = msg_components[1]
+
+        if user_id == from_user:
+            error_msg = "[WARNING] You can not LIKE yourself!"
+            from_user = "_" + from_user
+            raise grpc.RpcError(error_msg + from_user)
+
+        for user in self.allow_users:
+            if user_id == user['user_id'] and from_user not in self.allow_users[int(user_id)]["like_from"]:
+                self.allow_users[int(user_id)]['like_count'] += 1
+                self.allow_users[int(user_id)]['like_from'].append(from_user)
+                if self.allow_users[int(user_id)]['like_count'] >= 2:
+                    self.allow_users[int(user_id)]['is_allow'] = True
+
+        print("  {")
+        print("    user_id: ", self.allow_users[int(user_id)]["user_id"])
+        print("    like_count: ", self.allow_users[int(user_id)]["like_count"])
+        print("    like_from: ", self.allow_users[int(user_id)]["like_from"])
+        print("    is_allow: ", self.allow_users[int(user_id)]["is_allow"])
+        print("  }")
+
+    def SendMessage(self, request, context):
+        '''
+        --- PSEUDO CODE ---
+        if LIKE msg:
+            user[i].like_count += 1
+            if user[i].like_count >= 2:
+                user[i].is_allow = True
+        else:
+            if user[i].is_allow == True:
+                Send message
+                user[i].like_count = 0
+                user[i].is_allow = False
+            else:
+                return "You are not allowed to send message"
+        '''
+
+        print('--------- Server - Send Request ---------------------')
+        # if request of new user (dont have id)
+        if not request.user.id:
+            print("[CHECK] Is NEW user: True")
+            request.user.id = self.CreateNewUser()
+        else:
+            print("[CHECK] Is NEW user: False")
+
+        if self.IsLikeMessage(request.msg):
+            # if LIKE msg, still allow this user send msg
+            self.HandleLikeMessage(request.msg, request.user.id)
+        else:
+            # if user is allowed to send message
+            if self.allow_users[int(request.user.id)]['is_allow']:
+                print("[CHECK] Is ALLOW send msg: True")
+                # add message
+                self.messages.append(request)
+
+                # reset user state
+                self.allow_users[int(request.user.id)]['like_count'] = 0
+                self.allow_users[int(request.user.id)]['is_allow'] = False
+            else:
+                print("[CHECK] Is ALLOW send msg: False")
+                error_msg = "[WARNING] You are NOT allowed to send message"
+                from_user = "_" + request.user.id
+                raise grpc.RpcError(error_msg+from_user)
+
+        print("[UPDATE] User state ")
+        print("  {")
+        print("    user_id: ", self.allow_users[int(
+            request.user.id)]["user_id"])
+        print("    like_count: ", self.allow_users[int(
+            request.user.id)]["like_count"])
+        print("    is_allow: ", self.allow_users[int(
+            request.user.id)]["is_allow"])
+        print("  }")
+
+        print("[SEND] Message")
+        print("  user {")
+        print("    id: ", request.user.id)
+        print("    name: ", request.user.name)
+        print("  }")
+        print("  msg: ", request.msg)
+        print("  time: ", request.time)
+
         return request
 
     def ReceiveMessage(self, request, context):
@@ -43,12 +160,6 @@ def serve():
     print("Server started")
 
     server.wait_for_termination()
-
-    # try:
-    #     while True:
-    #         time.sleep(86400)
-    # except KeyboardInterrupt:
-    #     server.stop(0)
 
 
 if __name__ == '__main__':
